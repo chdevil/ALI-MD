ea74b37840675b5a53b739f4b3f464ff861b6f92
chdevil <146427458+chdevil@users.noreply.github.com>
2025-05-17 15:48:47 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "BWM-XMD;;;H4sIAAAAAAAAA61UW46jRhTdS30lsjXGvLHUUjAGg98NfuEoH2UocNk8iwIbjywlO4mykexlVhJhd8+0kslMRwofqKgq7j3n3HPvR5CkuEBjVIPeR5ARXEGKmiWtMwR6oF8GASKgDXxIIegBr89fzwttJi131pqwaGGcaJbOdnOaqspm38de4hFxxW859wnc2iAr9xH2vhFw2HLP+5Up+DvoOuVweWVG6iXC89xjRdVfblpKjgcJ098lpydwayJCTHAS6tkBxYjAaIzqBcTkffCHGi9aTDJUsTqttmZanVbeCotCGTrPRm3wHNEt7thxt9E74bPnfRbZBEtKeh21dkimF+64i/3+chVNpNMod4PzMqHM+a7HrQ0KHCbIt3yUUEzrd+seDtb7TIAdSTaWZ9KhF5sn0UauRX/ijuzBwmQrNuGXgX5i3gl8EEjWDgV2aayn7kWxJ5I6pYTph1xgc27EudfBaezy1ZF5C3xBXr1y+i+6e9ZcN4bxPFPljcrPntm+lu2TIp5LrH2aDVxPTXNtZq6DRH8f/OUWcmZmtDqLWYqkqXTJBXVbrgeTpRizSYo8pyMVna46ddQv8CEtybdQzoxkM+vHRiuYhM+bYcmq5tpdI+bs5c4oPgppS/HHfWrPp954McrF+KC3PDu0KZaXXOZus7FhpuOR2F3AWloexYNy1cLnpzujE6otH/S6tzYgKMQFJZDiNLnvCXwbQL9ykEcQvcsLdvK6DquukllKlCe2oEnPRIahq1WYmvlsZk6LkW/k1tKTn0AbZCT1UFEg38QFTUk9RUUBQ1SA3s/3SjWkCYpTikbYBz2gsJzEiLyssN3uT8WH8wHSAmbZhwRR0AYBSeMpAj1KStQG9x94oS+zvKj0OUGRZEZUREmUBFkaaANF6+qDhmL8SLrEMSoojDPQ60q8xEvNvVv7/8FhMIyqqwzfZVlBUyRO7LIyy6qipPEKpwv6d3H80gYJutCHjxv1uW4bBJgUdJWUWZRC/9Xkr4fQ89IyoU6deFqzQAT03mwjSnESFg2zMoHEO+AKaQ0P0AtgVKDPBUcE+a9cXoaYlvqNDzfzjbExnD5osDeB/qFN72vyRPdrLCOwAit3WVkUBIVrbjYHbZDAJhj44dNvv3/69Y/7+8/P6x8boV4oNBl9RCGOCtAD2qLmBZQa+vRopZ4+HKqrUNVCFXyh/NpKD6ueN5UnGN71RCbDfBZXknzljHN1ZNmSWMKkFUl6OJQTM+verfr3IKAH9FFrq7fO16pUWplabPaH4wBuIXaMIjfNq3MO7ee57++k4dRV9LkpGoJubJLuyM4ZZVfqUfcg8GMqsKaouJexOLPIWDs/Ndl8VGEPvU3WUhyOLxf2Aob5cL61inqmbVeHwyka6cm13LmK4JhDZC5aJ20px8Fpisfny7Y1sbecPXKwre+5BNUWjdnK3w1nZJHsQ/XR5PchE70Md/zSfvj+GWB0n5UvVflucR/AGw8yt/abGC/T918mWN8zCDe3t1NhXF/y4460OlPl4k14hZK8njudDF2sfjW6iH4Abk0zZBGkQUpi0AMw8Ul6tw5Jy8bUVhKk30im9S1LDR/MI1hQ9UujfK33+MetBUkzExaHxrprScpXjetrNcscCulr3wG1eYyBA25/AaPX1UqiCAAA",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -27,7 +27,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "923003588997",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "923706489211",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "ALI",
 // add bot owner name 
@@ -77,7 +77,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "923003588997",
+DEV: process.env.DEV || "923706489211",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
